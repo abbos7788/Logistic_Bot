d7722c158fe779c73db5946cee8e91c9d1431d77
Abbos Nazarov <104083967+abbos7788@users.noreply.github.com>
2022-06-16 14:56:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import requests
-TOKEN = '5338605138:AAGb92hX74NemdypTuGIW5q9aarZ2oFMx1Y'
-chat_id = '-1001794987264'
-chat_id2 = '-1001722090137'
+TOKEN = 'My_Token'
+chat_id = 'group_id'
+chat_id2 = 'channel_id'
